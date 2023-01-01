cd75b5542d3477fb13d4ac395cf926a88a1c826e
Mayavi333 <105354161+Mayavi333@users.noreply.github.com>
2023-01-01 15:04:27 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,13 +3,13 @@ class script(object):
 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, ɪ ᴄᴀɴ ᴩʀᴏᴠɪᴅᴇ ᴍᴏᴠɪᴇꜱ, ᴊᴜꜱᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ᴛᴏᴜʀ ɢʀᴏᴜᴩ ᴀɴᴅ ᴇɴᴊᴏy😍"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/LUTTAPPIxTG>『🍁』༺⋆ͥ⋆ͣ⋆᭄ͫ⁣𓆩𝙻𝚄𝚃𝚃𝙰𝙿𝙿𝙸𓆪➳࿐𝆺𝅥⃝🇧🇷</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: ᴩyʀᴏɢʀᴀᴍ
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: ᴩyᴛʜᴏɴ 3
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: ᴍᴏɴɢᴏ ᴅʙ
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: ᴋᴏyᴇʙ
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+    ABOUT_TXT = """<b>🌝𝙼𝚈 𝙽𝙰𝙼𝙴🌝: {}
+📍𝙲𝚁𝙴𝙰𝚃𝙾𝚁📍: <a href=https://t.me/LUTTAPPIxTG>『🍁』༺⋆ͥ⋆ͣ⋆᭄ͫ⁣𓆩𝙻𝚄𝚃𝚃𝙰𝙿𝙿𝙸𓆪➳࿐𝆺𝅥⃝🇧🇷</a>
+😎Lɪʙʀᴀʀʏ😎: ᴩyʀᴏɢʀᴀᴍ
+🌟Lᴀɴɢᴜᴀɢᴇ🌟: ᴩyᴛʜᴏɴ 3
+🗃️DᴀᴛᴀBᴀsᴇ🗃️: ᴍᴏɴɢᴏ ᴅʙ
+⚙️Bᴏᴛ Sᴇʀᴠᴇʀ⚙️: ᴋᴏyᴇʙ
+✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]</b>"""
     SOURCE_TXT = """<b>NOTE:</b>
 - Join our new movies updates channel» @KOMOFFICIAL OUR ALL CHANNELS AND GROUPS» @KOM_LINKS  ©@KOMBOTZZ.
 - Support - https://t.me/KINGOFMOVIES003
@@ -91,11 +91,11 @@ This module only works for my admins
 • /unban  - <code>to unban a user.</code>
 • /channel - <code>to get list of total connected channels</code>
 • /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
+    STATUS_TXT = """🗃️Tᴏᴛᴀʟ Fɪʟᴇs🗃️: <code>{}</code>
+💝Tᴏᴛᴀʟ Usᴇʀs💝: <code>{}</code>
+⚜️Tᴏᴛᴀʟ Cʜᴀᴛs⚜️: <code>{}</code>
+〽️Usᴇᴅ Sᴛᴏʀᴀɢᴇ〽️: <code>{}</code> 𝙼𝚒𝙱
+♾️Fʀᴇᴇ Sᴛᴏʀᴀɢᴇ♾️: <code>{}</code> 𝙼𝚒𝙱"""
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
